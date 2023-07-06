b9ec872d963698ece4b8d3e9d2fb423539c726a1
blacknii <42908977+blacknii@users.noreply.github.com>
2023-07-06 11:23:00 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 // import MuiTypography from "./components/MuiTypography";
-import MuiButton from "./components/MuiButton";
+// import MuiButton from "./components/MuiButton";
+import MuiTexField from "./components/MuiTexField";
 
 function App() {
   return (
     <>
       {/* <MuiTypography /> */}
-      <MuiButton />
+      {/* <MuiButton /> */}
+      <MuiTexField />
     </>
   );
 }
